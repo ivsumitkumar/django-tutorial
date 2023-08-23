4425fac2b4f724efa3dd6303de653a2f9a139ecb
sumit kumar <vishnusumit2@gmail.com>
2023-08-23 14:14:24 +0530

    first django app part 2 completed

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,22 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    publication_date = models.DateField("date published")
+
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.publication_date >= timezone.now()- datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField( max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
